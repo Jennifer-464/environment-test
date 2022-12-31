a17e17cdab742cd7a3405023b39362b59e081d99
Jennifer Lopez <jenniferlopezh0@gmail.com>
2022-12-30 18:58:24 -0800

    completed the layout for node js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("smoll test");
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.static('public'));
+
+
+router.get('/',function(req,res){
+    res.render(path.join(__dirname+'/views/home.ejs'));
+});
+
+app.use('/', router);
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on Port ${port}`));
